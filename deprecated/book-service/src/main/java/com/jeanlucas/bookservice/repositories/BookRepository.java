1e645848f0b26e106818b2ea6443e360e184adf2
jeanlucasbs <jean.silva@discente.ufg.br>
2023-10-01 20:15:35 -0300

    delete folder deprecared

--- deprecated/book-service/src/main/java/com/jeanlucas/bookservice/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jeanlucas.bookservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jeanlucas.bookservice.entities.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
